17d3a458c00278ed444f5e15a303500f663eb066
ahtesham007 <zsyedahtesham@yahoo.com>
2023-01-05 14:02:05 +0530

    max sub array solution

---
+++ arrays/max_subarray.py
@@ -0,0 +1,37 @@
+
+def maxSubarray(nums):
+    curr_sum = 0
+    max_sum = nums[0]
+
+    for i in range(len(nums)):
+        curr_sum += nums[i]
+
+        if curr_sum > max_sum:
+            max_sum = curr_sum
+        
+        if curr_sum < 0:
+            curr_sum = 0
+    
+    return max_sum
+
+def printMaxSubarray(nums):
+    curr_sum = 0
+    max_sum = nums[0]
+
+    s = 0
+    e = 0
+
+    for i in range(len(nums)):
+        curr_sum += nums[i]
+
+        if curr_sum > max_sum:
+            max_sum = curr_sum
+            e = i
+        
+        if curr_sum < 0:
+            curr_sum = 0
+            s = i + 1
+    
+    return max_sum,nums[s:e+1]
+
+print(printMaxSubarray([5,4,-1,7,8]))
