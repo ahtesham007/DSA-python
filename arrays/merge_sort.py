d9d0208b0d57dde56195c67f5d1a732a24cf8e1c
ahtesham007 <zsyedahtesham@yahoo.com>
2023-01-09 17:51:49 +0530

    merge sort solution with testcases added

--- arrays/merge_sort.py
+++ arrays/merge_sort.py
@@ -27,3 +27,15 @@ def merge(left, right):
 arr = [5, 2, 7, 3, 1, 8, 4, 6]
 sorted_arr = merge_sort(arr)
 print(sorted_arr)  # [1, 2, 3, 4, 5, 6, 7, 8]
+
+def test_merge_sort():
+  assert merge_sort([5]) == [5]
+  assert merge_sort([1, 2]) == [1, 2]
+  assert merge_sort([2, 1]) == [1, 2]
+  assert merge_sort([5, 2, 7, 3, 1]) == [1, 2, 3, 5, 7]
+  assert merge_sort([8, 4, 6, 1, 3, 5, 7, 2]) == [1, 2, 3, 4, 5, 6, 7, 8]
+  assert merge_sort([1, 2, 3, 4, 5, 6, 7, 8]) == [1, 2, 3, 4, 5, 6, 7, 8]
+  assert merge_sort([8, 7, 6, 5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5, 6, 7, 8]
+  print("All test cases pass.")
+
+test_merge_sort()
