d8e83ca9cc248835ad89936855f906c4d8b09db2
ahtesham007 <zsyedahtesham@yahoo.com>
2023-01-16 19:01:38 +0530

    add combination solution for grid unique path problem

--- arrays/grid_unique_path.py
+++ arrays/grid_unique_path.py
@@ -10,18 +10,29 @@ def countPathRecursive(i, j, m, n):
         return countPathRecursive(i+1, j, m, n) + countPathRecursive(i, j+1, m, n)
 
 # Dynamic Programming
-def uniquePaths(m: int, n: int) -> int:
+def uniquePathsdp(m: int, n: int) -> int:
     dp = [[-1]*n for _ in range(m)]
     return countPath(0, 0, m, n, dp)
 
-def countPath(i, j, m, n, dp):
+def countPathdp(i, j, m, n, dp):
     if i == m-1 and j == n-1: return 1
     if i >= m or j >= n:
         return 0
     if dp[i][j] != -1:
         return dp[i][j]
     else:
-        dp[i][j] = countPath(i+1, j, m, n, dp) + countPath(i, j+1, m, n, dp)
+        dp[i][j] = countPathdp(i+1, j, m, n, dp) + countPathdp(i, j+1, m, n, dp)
         return dp[i][j]
 
-print(uniquePaths(2, 3))
+# Combination Approach
+def uniquePaths(m: int, n: int) -> int:
+    steps = m + n - 2
+    
+    denominator = m - 1
+    res = 1
+    for i in range(1, denominator + 1):
+        res = res * (steps - denominator + i) // i
+    return res              
+
+
+print(uniquePaths(3, 7))
